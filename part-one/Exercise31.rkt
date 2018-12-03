9a8e88f0b7d1bef7b5d8a25ea600af4971eb2d2f
Shiraz Suleman <shirazsuleman@gmail.com>
2018-12-03 20:41:21 +0200

    Exercise 31.

---
+++ part-one/Exercise31.rkt
@@ -0,0 +1,48 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise31) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
+; Exercise 31
+
+(require 2htdp/batch-io)
+
+(define (letter fst lst signature-name)
+  (string-append
+    (opening fst)
+    "\n\n"
+    (body fst lst)
+    "\n\n"
+    (closing signature-name)))
+ 
+(define (opening fst)
+  (string-append "Dear " fst ","))
+ 
+(define (body fst lst)
+  (string-append
+   "We have discovered that all people with the" "\n"
+   "last name " lst " have won our lottery. So, " "\n"
+   fst ", " "hurry and pick up your prize."))
+ 
+(define (closing signature-name)
+  (string-append
+   "Sincerely,"
+   "\n\n"
+   signature-name
+   "\n"))
+
+(write-file 'stdout (letter "Rob" "Bob" "Mr. Anderson"))
+(write-file 'stdout (letter "John" "Smith" "Mr. Anderson"))
+(write-file 'stdout (letter "Sean" "Black" "Mr. Anderson"))
+
+(define (main in-fst in-lst in-signature out)
+  (write-file out
+              (letter (read-file in-fst)
+                      (read-file in-lst)
+                      (read-file in-signature))))
+
+(write-file "fst.dat" "Jim\n")
+(write-file "lst.dat" "Johnson\n")
+(write-file "signature.dat" "Mr. Anderson\n")
+
+(main "fst.dat" "lst.dat" "signature.dat" "sample.dat")
+
+(write-file 'stdout (read-file "sample.dat"))
